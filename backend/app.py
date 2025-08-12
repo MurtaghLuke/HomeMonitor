1823b36664c0862429a45b803d10a311fcc05a4f
MurtaghLuke <l.murtagh2m@gmail.com>
2025-08-12 17:51:55 +0100

    crerate flask app and add mongo db details

--- backend/app.py
+++ backend/app.py
@@ -3,3 +3,17 @@ from datetime import datetime, timezone
 from pymongo import MongoClient, DESCENDING
 from dotenv import load_dotenv
 import os, pathlib
+
+
+
+#//  connect to mongo db
+client = MongoClient(MONGO_URL)
+db = client[DB_NAME]
+col = db[COLLECTION]
+
+# create flask app
+app = Flask(
+    __name__,
+    static_folder="../web",      #  serves index.html
+    static_url_path="/"          # serves the web folder
+)
